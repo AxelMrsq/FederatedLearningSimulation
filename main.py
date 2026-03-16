302f8ca31dd1663d7215601543df6ef8558dbaa6
Axel Marsacq <axel.marsacq2@mail.dcu.ie>
2026-03-16 14:24:24 +0100

    Iteration

--- main.py
+++ main.py
@@ -29,3 +29,24 @@ client_1.train_local_model(train_ds_client1_set1, val_ds_client1_set1)
 
 # first local iteration training on client 2
 client_2.train_local_model(train_ds_client2_set1, val_ds_client2_set1)
+
+#https://www.tensorflow.org/api_docs/python/tf/keras/Layer#get_weights
+agg.aggregate_weights(client_1.model.get_weights(), client_2.model.get_weights())
+
+
+# save initial global weights
+agg.save_global_weights()
+
+
+# load the client1 model with the initial global weights
+client_1.update_weights("./global_weights_iter/iter1.weights.h5")
+
+# load the client1 model with the initial global weights
+client_2.update_weights("./global_weights_iter/iter1.weights.h5")
+
+
+# first local iteration training on client 1
+client_1.train_local_model(train_ds_client1_set2, val_ds_client1_set2)
+
+# first local iteration training on client 2
+client_2.train_local_model(train_ds_client2_set2, val_ds_client2_set2)
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from model import global_model
 import os
 #https://docs.python.org/3/library/os.html
 
-
+import numpy
 
 # aggregator class represent the server with the global model
 class Aggregator:
@@ -43,6 +43,23 @@ class Aggregator:
         self.model.save_weights(f"./global_weights_iter/iter{self.iter}.weights.h5")
         
         #update training iteration
-        self.iter += 0
+        self.iter += 1
+    
+    def aggregate_weights(self, client1_weights, client2_weights) : #aggregate the weights of the two clients with a mean
+
+        aggregated_weights = []
+        for weights_layer in client1_weights :
+            aggregated_weights.append(numpy.zeros_like(weights_layer))
+
+        for i in range(len(aggregated_weights)) :
+            aggregated_weights[i] = client2_weights[i] + client1_weights[i]
+        
+        final_weights = []
+        for weights_layer in aggregated_weights :
+            final_weights.append(weights_layer / 2)
+        
+        #https://www.tensorflow.org/api_docs/python/tf/keras/Layer#set_weights
+        self.model.set_weights(final_weights)
+        
 
 
